8b0d2994d129231e0e8704dfaa76f66372b856f2
Karen Dahl Aarhus <karen.aarhus@bekk.no>
2023-10-16 15:24:38 +0200

    add theme styling

---
+++ src/styles/themes.js
@@ -0,0 +1,51 @@
+const palette = {
+  tollmorkebla: '#12404F',
+  tollturkis: '#007682',
+  tollgul: '#FFCC05',
+  tolloransje: '#F25A22',
+  emblemrod: '#EF3E42',
+  emblemgul: '#FFD200',
+  havbla: '#0097D6',
+  magentadyp: '#DA1C5C',
+  marinebla: '#0054A6',
+  purpur: '#942977',
+  lightgrey: '#f5f5f6',
+};
+
+export const theme = {
+  colors: {
+    background: palette.lightgrey,
+    primary: palette.tollmorkebla,
+    secondary: palette.tollturkis,
+    danger: palette.tollgul,
+    failure: palette.tolloransje,
+  },
+  spacing: {
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
+  },
+  textVariants: {
+    header: {
+      fontFamily: 'Roboto',
+      fontSize: 36,
+      fontWeight: 'bold',
+    },
+    body: {
+      fontFamily: 'Roboto',
+      fontSize: 16,
+      fontWeight: 'light',
+    },
+  },
+};
+/* 
+export const darkTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    background: palette.black,
+    foreground: palette.white,
+  },
+};
+ */
