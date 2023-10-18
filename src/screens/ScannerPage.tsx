d0a65fa9ea88b6a9658a8a0930cf6dc26d9ba888
Vegard Smines <vegard.smines@bekk.no>
2023-10-18 15:23:20 +0200

    added websockets form update

--- src/screens/ScannerPage.tsx
+++ src/screens/ScannerPage.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
-import {scanId} from '../utils/BlinkIdScanner';
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button, Linking} from 'react-native';
+import {getFormId} from '../utils/getFormId';
+import {handleScan} from '../utils/handleScan';
+import {postData} from '../services/postData';
+
 const ScannerPage = () => {
-  const handleScan = async () => {
-    try {
-      const scanResult = await scanId();
-      console.log('Scan Result:', scanResult);
-    } catch (error) {
-      console.error('Scanning failed', error);
-    }
+  const [formId, setFormId] = useState('1234');
+
+  const testData = {
+    data: {
+      name: 'John Doe',
+      email: 'JohnDoe@mail.com',
+      phone: '48191919',
+      address: '123 Main St',
+      country: 'Norway',
+    },
+    formId,
   };
+
+  useEffect(() => {
+    const unsubscribe = Linking.addEventListener('url', getFormId(setFormId));
+
+    Linking.getInitialURL()
+      .then(url => {
+        if (url) {
+          getFormId(setFormId)({url});
+        }
+      })
+      .catch(err => console.error('An error occurred', err));
+
+    return () => {
+      unsubscribe.remove();
+    };
+  }, []);
 
   return (
     <View>
-      <Text>Scanner Page</Text>
+      <Text>Scanner Page FormId: {formId}</Text>
       <Button title="Scan ID" onPress={handleScan} />
+      <View style={{height: 40}} />
+      <Button title="Post Data" onPress={() => postData(testData)} />
     </View>
   );
 };
---
+++ types/testData.d.ts
@@ -0,0 +1,10 @@
+export type TestData = {
+  data: {
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    country: string;
+  };
+  formId: string;
+};
