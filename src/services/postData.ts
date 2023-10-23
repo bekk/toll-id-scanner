4bddac7edd80a82d28bc09b6bc67929d1b8a90f5
Karen Dahl Aarhus <karen.aarhus@bekk.no>
2023-10-23 10:34:10 +0200

    check for different browsers

--- src/services/postData.ts
+++ src/services/postData.ts
@@ -4,6 +4,8 @@ import {ScanResultType} from '@typedefs/ScanResultType';
 export const postData = async (data: ScanResultType): Promise<void> => {
   try {
     console.log(data);
+    const BROWSERS_TO_TRY = ['googlechrome://', 'firefox://', 'safari://'];
+
     await fetch(`http://${process.env.IP_ADDRESS}:8082/data/`, {
       method: 'POST',
       headers: {
@@ -13,8 +15,18 @@ export const postData = async (data: ScanResultType): Promise<void> => {
     }).then(response => console.log(response));
     if (Platform.OS === 'android') {
       BackHandler.exitApp();
-    } else {
-      Linking.openURL('googlechrome://');
+    } else if (Platform.OS === 'ios') {
+      const browserFound = BROWSERS_TO_TRY.some(async browserURL => {
+        if (await Linking.canOpenURL(browserURL)) {
+          Linking.openURL(browserURL);
+          return true;
+        }
+        return false;
+      });
+
+      if (!browserFound) {
+        console.log('Please go back to the original window');
+      }
     }
   } catch (error) {
     console.error('Posting failed:', error);
